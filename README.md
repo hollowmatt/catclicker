d1515945d9e1ac84cd699e467e4831297045c0b5
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-04-09 20:58:56 -0700

    Initial commit: setup of the folders, and JS/CSS stuff

---
+++ README.md
@@ -0,0 +1,4 @@
+Cat Clicker Project
+-------------------
+## What
+just a plain HTML, JS and CSS app to count clicks on a cat
---
+++ js/clicker.js
@@ -0,0 +1,7 @@
+// clicker.js
+// This is the main JS file for the project
+
+// jQuery event handler for click event
+$('#my-elem').click(function(e) {
+  console.log('clicked');
+});
