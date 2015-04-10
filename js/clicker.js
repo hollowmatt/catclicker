505b8813a57f0885ea520661761f784a615b873a
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-04-09 21:11:32 -0700

    Added kitten picture, and counter for click

--- js/clicker.js
+++ js/clicker.js
@@ -2,6 +2,9 @@
 // This is the main JS file for the project
 
 // jQuery event handler for click event
+var CLICK_COUNT = 0;
 $('#my-elem').click(function(e) {
-  console.log('clicked');
+  CLICK_COUNT += 1;
+  console.log('clicked ' + CLICK_COUNT + ' times');
+  $("#count").text("Clicked " + CLICK_COUNT + " times");
 });
