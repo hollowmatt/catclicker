59732411e094e6309759ad3e0bbe5bb27371bc82
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-04-12 14:56:22 -0700

    Added second cat
    :

--- js/clicker.js
+++ js/clicker.js
@@ -2,9 +2,17 @@
 // This is the main JS file for the project
 
 // jQuery event handler for click event
-var CLICK_COUNT = 0;
-$('#my-elem').click(function(e) {
-  CLICK_COUNT += 1;
-  console.log('clicked ' + CLICK_COUNT + ' times');
-  $("#count").text("Clicked " + CLICK_COUNT + " times");
+var CAT1_CLICK_COUNT = 0;
+var CAT2_CLICK_COUNT = 0;
+
+$('#cat1').click(function(e) {
+  CAT1_CLICK_COUNT += 1;
+  console.log('clicked ' + CAT1_CLICK_COUNT + ' times');
+  $("#cat1-count").text("Clicked " + CAT1_CLICK_COUNT + " times");
+});
+
+$('#cat2').click(function(e) {
+  CAT2_CLICK_COUNT += 1;
+  console.log('clicked ' + CAT2_CLICK_COUNT + ' times');
+  $("#cat2-count").text("Clicked " + CAT2_CLICK_COUNT + " times");
 });
