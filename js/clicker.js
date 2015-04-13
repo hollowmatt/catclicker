7d986ac4ec205005a47001f346fe0246ef9e1499
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-04-13 06:49:55 -0700

    modify to use plain js instead of jquery

--- js/clicker.js
+++ js/clicker.js
@@ -13,12 +13,22 @@ for (var i = 0; i < CATS; i++) {
   html += '<p id="cat' + cat + '-count">';
   html += 'Clicked 0 times </p>';
   html += '<img class="img-responsive" id="cat' + cat + '" src="img/cat' + cat + '.jpg"></img>';
-  $('#cats').append(html);
-  console.log(eventID);
-  $(eventID).click(cat,function(e) {
-    CLICK_COUNT["cat" + cat] ++;
-    var whichCat =eventID +'-count';
-    alert(whichCat + ", " + cat + ", " + CLICK_COUNT["cat" + cat]);
-    $(whichCat).text("Clicked " + CLICK_COUNT["cat" + cat] + " times");
-  });
+  var node = document.getElementById('cats');
+  var catdiv = document.createElement('div');
+  catdiv.setAttribute("id", "cat" + cat);
+  catdiv.innerHTML = 'new div';
+  node.appendChild(catdiv);
+  //$('#cats').append(html);
+  elem = document.getElementById("cat" + cat);
+  elem.addEventListener('click'), (function(catCopy){
+    return function() {
+      alert(catCopy);
+    }
+  })(cat);
+
+  // $(eventID).click(cat,function(e) {
+  //   CLICK_COUNT["cat" + cat] ++;
+  //   var whichCat =eventID +'-count';
+  //   $(whichCat).text("Clicked " + CLICK_COUNT["cat" + cat] + " times");
+  // });
 }
