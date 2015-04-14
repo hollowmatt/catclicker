3960f6716a611457ddd464824c36ca006fb1f398
Matthew Holloway <mholloway@book.com>
2015-04-14 10:19:35 -0700

    Fixed up the cat click in the jQuery method of master (as opposed to the purejs branch - which actually isn't pure)

--- js/clicker.js
+++ js/clicker.js
@@ -8,17 +8,24 @@ var CLICK_COUNT = { "cat1": 0, "cat2" : 0 };
 for (var i = 0; i < CATS; i++) {
   var cat = i + 1;
   var eventID = '#cat' + cat;
+  $('#cats').append(htmlString(cat));
+  console.log(eventID);
+  (function(catCopy){
+    var node = "cat" + catCopy;
+    $("#" + node).click(catCopy,function(e) {
+      CLICK_COUNT["cat" + catCopy] ++;
+      var whichCat = node + '-count';
+      $("#" + whichCat).text("Clicked " + CLICK_COUNT["cat" + catCopy] + " times");
+    });
+  })(cat);
+}
+
+// HTML builder
+function htmlString(cat) {
   var html = '<div id=' + "cat" + cat + ' class="col-md-6">';
   html += '<h2>' + "Cat " + cat + '</h2>';
   html += '<p id="cat' + cat + '-count">';
   html += 'Clicked 0 times </p>';
   html += '<img class="img-responsive" id="cat' + cat + '" src="img/cat' + cat + '.jpg"></img>';
-  $('#cats').append(html);
-  console.log(eventID);
-  $(eventID).click(cat,function(e) {
-    CLICK_COUNT["cat" + cat] ++;
-    var whichCat =eventID +'-count';
-    alert(whichCat + ", " + cat + ", " + CLICK_COUNT["cat" + cat]);
-    $(whichCat).text("Clicked " + CLICK_COUNT["cat" + cat] + " times");
-  });
+  return html;
 }
