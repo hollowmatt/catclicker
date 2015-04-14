a071e9c9e13fa89f3d2ce51024e23a1aa721f535
Matthew Holloway <mholloway@book.com>
2015-04-14 10:11:08 -0700

    Cleaned up the JS file

--- js/clicker.js
+++ js/clicker.js
@@ -1,29 +1,21 @@
 // clicker.js
 // This is the main JS file for the project
 
-// jQuery event handler for click event
 var CATS = 2;
 var CLICK_COUNT = { "cat1": 0, "cat2" : 0 };
 
 for (var i = 0; i < CATS; i++) {
   var cat = i + 1;
-  var eventID = '#cat' + cat;
-  var html = '<div id=' + "cat" + cat + ' class="col-md-6">';
-  html += '<h2>' + "Cat " + cat + '</h2>';
-  html += '<p id="cat' + cat + '-count">';
-  html += 'Clicked 0 times </p>';
-  html += '<img class="img-responsive" id="cat' + cat + '" src="img/cat' + cat + '.jpg"></img>';
   var node = document.getElementById('cats');
   var catdiv = createDiv("cat" + cat);
   node.appendChild(catdiv);
-  elem = document.getElementById("cat" + cat);
-  var catHeader = document.createElement('h2');
-  catHeader.innerHTML = "Cat " + cat;
-  elem.appendChild(catHeader);
+  
+  var elem = document.getElementById("cat" + cat);
+  elem.appendChild(createH2("Cat " + cat));
   elem.appendChild(createPara("cat" + cat));
   elem.appendChild(createImg("cat" + cat));
-  var clickNode = document.getElementById("img-cat" + cat);
   
+  var clickNode = document.getElementById("img-cat" + cat);
   (function(catCopy){
     $(clickNode).click(catCopy,function(e) {
       CLICK_COUNT["cat" + catCopy] ++;
@@ -33,6 +25,7 @@ for (var i = 0; i < CATS; i++) {
   })(cat);
 }
 
+// HTML Creator functions
 function createDiv(id) {
   var newDiv = document.createElement('div');
   newDiv.setAttribute("id", id);
@@ -53,5 +46,11 @@ function createImg(id) {
   newImg.setAttribute("class", 'img-responsive');
   newImg.setAttribute("src", "img/" + id + ".jpg");
   return newImg;
+}
+
+function createH2(innerText) {
+  var newH2 = document.createElement('h2');
+  newH2.innerHTML = innerText;
+  return newH2;
 }
 
