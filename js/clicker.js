aff6a13537b27ac07ac15858d268caf88185cecb
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-04-15 20:42:41 -0700

    Adding more cats and nav

--- js/clicker.js
+++ js/clicker.js
@@ -2,12 +2,38 @@
 // This is the main JS file for the project
 
 // jQuery event handler for click event
-var CATS = 2;
-var CLICK_COUNT = { "cat1": 0, "cat2" : 0 };
+var CATS = [
+  {
+    "name" : "Oscar",
+    "image" : "cat1.jpg",
+    "clicks" : 0
+  },
+  {
+    "name" : "Fred",
+    "image" : "cat2.jpg",
+    "clicks" : 0
+  },
+  {
+    "name" : "Peach and Fuzz",
+    "image" : "cat3.jpg",
+    "clicks" : 0
+  },
+  {
+    "name" : "Bubbles",
+    "image" : "cat4.jpg",
+    "clicks" : 0
+  },
+  {
+    "name" : "Minnie",
+    "image" : "cat5.jpg",
+    "clicks" : 0
+  }]
 
-for (var i = 0; i < CATS; i++) {
+console.log(CATS.length);
+for (var i = 0; i < CATS.length; i++) {
   var cat = i + 1;
   var eventID = '#cat' + cat;
+  $('#nav').append(navMenuItem(i));
   $('#cats').append(htmlString(cat));
   console.log(eventID);
   (function(catCopy){
@@ -27,5 +53,12 @@ function htmlString(cat) {
   html += '<p id="cat' + cat + '-count">';
   html += 'Clicked 0 times </p>';
   html += '<img class="img-responsive" id="cat' + cat + '" src="img/cat' + cat + '.jpg"></img>';
+  return html;
+}
+
+function navMenuItem(cat) {
+  var html = "<li>";
+  html += "<a href='index.html' id='" + CATS[cat]['name'] + "'>" + CATS[cat]['name'] + "</a>";
+  html += "</li>";
   return html;
 }
